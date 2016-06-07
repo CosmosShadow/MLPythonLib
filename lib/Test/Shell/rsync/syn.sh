6e77824b629c33a8c14b20634cdcf715c2a1f987
lichen <lichenarthurml@gmail.com>
2016-06-07 17:44:49 +0800

    other

--- lib/Test/Shell/rsync/syn.sh
+++ lib/Test/Shell/rsync/syn.sh
@@ -1,1 +1,3 @@
 rsync -a --progress --exclude-from 'exclude.txt' server@192.168.1.173:/home/server/Documents/projects/NumberRecognition ./
+
+rsync -a --progress server@192.168.1.173:/home/server/Documents/projects/NumberRecognition/AttetionWithTorch/model4/model/training/simple_model_with_fake_image_2016_06_07_15_23_34.t7 .
