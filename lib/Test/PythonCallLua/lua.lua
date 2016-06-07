41e93d7c2440ca073c66b533da23a90bd46cfd84
lichenarthurdata@gmail.com <lichenarthurdata@gmail.com>
2016-06-07 14:03:10 +0800

    change

--- lib/Test/PythonCallLua/lua.lua
+++ lib/Test/PythonCallLua/lua.lua
@@ -4,5 +4,5 @@ a = {}
 
 function method(path)
 	print(path)
-	a = {key1='value1', key1='value2'}
+	a = {'value1','value2'}
 end
--- lib/Test/PythonCallLua/python.py
+++ lib/Test/PythonCallLua/python.py
@@ -4,5 +4,13 @@ import lupa
 
 lua = lupa.LuaRuntime()
 lua.execute("dofile 'lua.lua' ")
-method = self.lua.globals().method
+method = lua.globals().method
 method('path')
+
+a = lua.globals().a
+
+print sorted(a.values())
+print a
+print len(a)
+for i in range(len(a)):
+	print a[i+1]
