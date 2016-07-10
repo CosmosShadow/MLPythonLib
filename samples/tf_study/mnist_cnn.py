f8bf31b0b7ca1565650a035b2dc18a7c4e956b6b
lichen <lichenarthurml@gmail.com>
2016-07-11 00:02:05 +0800

    从写模块

--- samples/tf_study/mnist_cnn.py
+++ samples/tf_study/mnist_cnn.py
@@ -8,6 +8,7 @@ mnist = data_mnist.read_data_sets(one_hot=True)
 # 输入
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder("float", [None,10])
+keep_prob = tf.placeholder("float")
 
 # 模型
 def weight_variable(shape):
@@ -26,26 +27,23 @@ def conv(inp, kw, kh, ic, oc):
 	h = tf.nn.relu(conv2d(inp, W) + b)
 	p = max_pool_2x2(h)
 	return p
-def fc(input, input_size, output_size):
-	W_fc1 = weight_variable([input_size, output_size])
-	b_fc1 = bias_variable([output_size])
-	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-	return h_fc1
+def fc(inp, input_size, output_size):
+	W = weight_variable([input_size, output_size])
+	b = bias_variable([output_size])
+	outp = tf.matmul(inp, W) + b
+	return outp
 
 x_image = tf.reshape(x, [-1,28,28,1])
 conv1 = conv(x_image, 5, 5, 1, 32)
 conv2 = conv(conv1, 5, 5, 32, 64)
-h_pool2_flat = tf.reshape(conv2, [-1, 7*7*64])
-h_fc1 = fc(h_pool2_flat, 7*7*64, 1024)
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-output=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+fc1 = fc(tf.reshape(conv2, [-1, 7*7*64]), 7*7*64, 1024)
+fc1_relu = tf.nn.relu(fc1)
+fc1_drop = tf.nn.dropout(fc1_relu, keep_prob)
+fc2 = fc(fc1_drop, 1024, 10)
+output=tf.nn.softmax(fc2)
 
 cross_entropy = -tf.reduce_sum(y*tf.log(output))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
 
 # 测试
 correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(y,1))
@@ -54,7 +52,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 with tf.Session() as sess:
 	sess.run(tf.initialize_all_variables())
 
-	for i in range(50):
+	for i in range(100):
 		batch_x, batch_y = mnist.train.next_batch(50)
 		if i%10 == 0:
 			train_accuracy = accuracy.eval(feed_dict={x:batch_x, y: batch_y, keep_prob: 1.0})
