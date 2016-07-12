68c95834caecf9ee50c7512db522683efcc05655
lichen <lichenarthurml@gmail.com>
2016-07-12 14:23:19 +0800

    format

--- samples/tf_study/mnist_fc.py
+++ samples/tf_study/mnist_fc.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import cmtf.data.data_mnist as data_mnist
 
+# 数据
 mnist = data_mnist.read_data_sets(one_hot=True)
 
 #----------------模型----------------
@@ -14,14 +15,14 @@ b = tf.Variable(tf.zeros([10]))
 output = tf.nn.softmax(tf.matmul(x,W) + b)
 cross_entropy = -tf.reduce_sum(y*tf.log(output))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-# 测试
+# 正确率
 correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-# 训练与测试
-init = tf.initialize_all_variables()
+# run
 with tf.Session() as sess:
-	sess.run(init)
+	# init
+	sess.run(tf.initialize_all_variables())
 	# train
 	for i in range(1000):
 		batch_xs, batch_ys = mnist.train.next_batch(100)
